6e1cebeb7041723aae5b159670566be52c2d4c31
bhm349@myy.haaga-helia.fi <bhm349@myy.haaga-helia.fi>
2025-01-30 11:07:28 +0200

    finish 30/1

--- src/main/java/fi/haagahelia/bookstore/domain/Book.java
+++ src/main/java/fi/haagahelia/bookstore/domain/Book.java
@@ -1,12 +1,30 @@
 package fi.haagahelia.bookstore.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String title;
     private String author;
     private int publicationYear;
     private String isbn;
     private int price;
+
+    public Book() {};
+
+    public Book(String title, String author, int publicationYear, String isbn, int price) {
+        this.title = title;
+        this.author = author;
+        this.publicationYear = publicationYear;
+        this.isbn = isbn;
+        this.price = price;
+    }
 
     public Long getId() {
         return this.id;
--- src/main/java/fi/haagahelia/bookstore/web/BookController.java
+++ src/main/java/fi/haagahelia/bookstore/web/BookController.java
@@ -1,37 +1,31 @@
 package fi.haagahelia.bookstore.web;
 
-// import java.util.ArrayList;
-// import java.util.List;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-// import org.springframework.validation.BindingResult;
-// import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import fi.haagahelia.bookstore.domain.Book;
+import fi.haagahelia.bookstore.domain.BookRepository;
 
 @Controller
 public class BookController {
-	@RequestMapping(value = "/index", method = RequestMethod.GET, params = { "name", "age" })
-	public String sayHello(@RequestParam("name") String name, @RequestParam("age") int age, Model model) {
-		model.addAttribute("name", name);
-		model.addAttribute("age", age);
-		return "hello";
+	private BookRepository repository;
+
+	// constructor injection. Can only be one constructor then.
+	public BookController(BookRepository repository) {
+		this.repository = repository;
 	}
 
-	@RequestMapping(value = "/index", method = RequestMethod.GET)
-	public String addFriendNew(Model model) {
+	@RequestMapping(value = { "/", "/Booklist" })
+	public String bookList(Model model) {
+		model.addAttribute("Books", repository.findAll());
+		return "bookList";
+	}
+
+	@GetMapping("/index")
+	public String welcome(Model model) {
 		model.addAttribute("book", new Book());
 		return "book";
 	}
-
-	// @RequestMapping(value = "/index", method = RequestMethod.POST)
-	// public String friendListNew(Friend friend, Model model) {
-	// 	friendList.add(friend);
-	// 	model.addAttribute("friendList", friendList);
-	// 	return "friend";
-	// }
 }
